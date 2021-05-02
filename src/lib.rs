d45a010fe5b86ec02ecff6764253973b4d4e66ea
Richard Anaya <richard.anaya@gmail.com>
2021-05-01 17:33:16 -0700

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use serde::*;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 pub struct Visual {
     pub html: Option<String>,
     pub text: Option<String>,
     pub image: Option<String>,
     pub log: Option<String>,
+    pub markdown: Option<String>,
 }
 
 pub trait Visualize {
